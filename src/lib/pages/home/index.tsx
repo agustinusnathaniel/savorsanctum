9c2b8ad528f7c1506266c4d77419e42077206c78
Agustinus Nathaniel <berserk.nate@gmail.com>
2025-08-05 20:19:00 +0700

    refactor: enable search - finally
    shouldReload ftw

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -22,7 +22,6 @@ const Home = () => {
       navigate({
         to: '/',
         search: (prev) => ({ ...prev, keyword }),
-        reloadDocument: true,
       });
     },
     {
@@ -31,7 +30,7 @@ const Home = () => {
   );
 
   return (
-    <div className="flex flex-col md:flex-row gap-4">
+    <div className="grid lg:grid-cols-[1fr_3fr] gap-4">
       <div className="flex-1/4">
         <Card>
           <CardHeader>
@@ -52,34 +51,43 @@ const Home = () => {
       </div>
 
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {foodPlaces.map((entry) => (
-          <Card key={entry.id}>
-            <CardHeader>
-              <CardTitle>{entry.name}</CardTitle>
-              <div className="flex gap-2 flex-wrap">
-                {
-                  // @ts-ignore
-                  entry.tags.map((tag) => (
-                    <Badge key={tag.name} className="bg-orange-700 font-normal">
-                      {tag.name}
+        {foodPlaces
+          .filter((item) =>
+            (item.name as string)
+              .toLowerCase()
+              .includes((keyword as string).toLowerCase()),
+          )
+          .map((entry) => (
+            <Card key={entry.id}>
+              <CardHeader>
+                <CardTitle>{entry.name}</CardTitle>
+                <div className="flex gap-2 flex-wrap">
+                  {
+                    // @ts-ignore
+                    entry.tags.map((tag) => (
+                      <Badge
+                        key={tag.name}
+                        className="bg-orange-700 font-normal"
+                      >
+                        {tag.name}
+                      </Badge>
+                    ))
+                  }
+                </div>
+              </CardHeader>
+              <CardContent className="flex flex-col gap-2">
+                <div className="flex gap-1 items-center flex-wrap">
+                  <MapPin size={16} />
+                  {/* @ts-ignore */}
+                  {entry.location.map((loc) => (
+                    <Badge key={loc.name} variant="outline">
+                      {loc.name}
                     </Badge>
-                  ))
-                }
-              </div>
-            </CardHeader>
-            <CardContent className="flex flex-col gap-2">
-              <div className="flex gap-1 items-center flex-wrap">
-                <MapPin size={16} />
-                {/* @ts-ignore */}
-                {entry.location.map((loc) => (
-                  <Badge key={loc.name} variant="outline">
-                    {loc.name}
-                  </Badge>
-                ))}
-              </div>
-            </CardContent>
-          </Card>
-        ))}
+                  ))}
+                </div>
+              </CardContent>
+            </Card>
+          ))}
       </div>
     </div>
   );
