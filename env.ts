395b4228928e8d19e412c29651a562b2ad107211
Agustinus Nathaniel <berserk.nate@gmail.com>
2025-12-06 18:16:17 +0700

    feat: attach umami analytics

--- env.ts
+++ env.ts
@@ -7,5 +7,7 @@ export default defineConfig({
     VITE_NOTION_TOKEN: z.string().min(1),
     VITE_NOTION_CULINARIES_DATASOURCE_ID: z.string().min(1),
     VITE_NOTION_PRODUCTS_DATASOURCE_ID: z.string().min(1),
+    VITE_UMAMI_SCRIPT_URL: z.string().optional(),
+    VITE_UMAMI_WEBSITE_ID: z.string().optional(),
   },
 });
